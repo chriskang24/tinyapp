82ddfb754473a32556d05f739a5ab7c6e43139f9
Christopher Kang <christopherjkang24@gmail.com>
2021-01-18 21:26:17 +0000

    added shortURL links

--- express_server.js
+++ express_server.js
@@ -10,6 +10,24 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+
+// app.get("/", function(req, res) {
+
+//   const templateVars1 = { urls: urlDatabase };
+
+//   res.render('urls_index', templateVars1)
+// })
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 })
@@ -25,4 +43,3 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
