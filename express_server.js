eef1708188db22c3d37fd1bac1ea5194ee834480
Christopher Kang <christopherjkang24@gmail.com>
2021-01-18 23:05:09 +0000

    added url submission and POST function

--- express_server.js
+++ express_server.js
@@ -2,13 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+
+  return Math.random().toString(36).substr(2, 6)
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -20,17 +32,9 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
-// app.get("/", function(req, res) {
-
-//   const templateVars1 = { urls: urlDatabase };
-
-//   res.render('urls_index', templateVars1)
-// })
-
 app.get("/", (req, res) => {
   res.send("Hello!");
-})
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -39,6 +43,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
